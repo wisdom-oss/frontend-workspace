b9c3bb9fe239802d1132dcdd81027ce696da0911
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-02-26 15:39:59 +0100

    documented the config files

--- modules.ts
+++ modules.ts
@@ -1,2 +1,9 @@
+/**
+ * This file is used to re-export every wisdom modules.
+ *
+ * This is necessary so that the {@link ModuleProvider} is able to inject these
+ * modules.
+ */
+
 export * as example from "example";
 export * as waterUsageForecasts from "water-usage-forecasts";
--- sidebar.ts
+++ sidebar.ts
@@ -1,7 +1,14 @@
+/** This file is used to define the sidebar on the left side of the screen. */
+
 import {
   SideBarEntries
 } from "./wisdom_modules/core/src/app/frame/side-bar/side-bar-entries";
 
+/**
+ * This constant is used to inject language content into the translator.
+ *
+ * The keys to use the injected will use the namespace "sidebar".
+ */
 export const langs = {
   de_DE: {
     "water-usage": "Wasserverbrauch"
@@ -10,6 +17,12 @@ export const langs = {
     "water-usage": "Water Usage"
   }
 }
+
+/**
+ * This constant describes how the sidebar should directly look like.
+ *
+ * For further explanation check {@link SideBarEntries}.
+ */
 export const sidebar: SideBarEntries = [
   [
     "Proof of Concept", "rocket", [
