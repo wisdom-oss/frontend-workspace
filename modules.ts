3c761d9517db881812a3f7b15f16cab219697b89
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-09-05 17:02:24 +0200

    removed accidentally pushed files

--- modules.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This file is used to re-export every wisdom modules.
- *
- * This is necessary so that the {@link ModuleProvider} is able to inject these
- * modules.
- */
-
-export * as example from "example";
-export * as waterUsageForecasts from "water-usage-forecasts";
