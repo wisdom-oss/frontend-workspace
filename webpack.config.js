5a2ed4d4c073aaabf6061d534d8872c52aac016b
zoax3155 <jan.stueven1@uni-oldenburg.de>
2023-07-14 14:16:30 +0200

    takeCsv im Proxy angepasst

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   devServer: {
     proxy: {
+      "/api/takeCsv/*": {
+        target: "http://localhost:8000"
+      },
       "/api/*": {
         target: "https://wisdom-demo.uol.de",
         changeOrigin: true,
@@ -9,7 +12,7 @@ module.exports = {
         target: "https://wisdom-demo.uol.de",
         changeOrigin: true,
         logLevel: "error",
-      }
+      },
     },
-  }
-}
+  },
+};
