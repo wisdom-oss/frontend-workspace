9f458e069740baa8833e73402faaf0a4dc81067a
zoax3155 <stueven.jan@web.de>
2023-08-15 16:04:54 +0200

    bws docker now reachable

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,15 @@ module.exports = {
         target: "http://localhost:8000",
       },
       // proxy for the local be water smart container
-      "/api/bws/debug": {
-        target: "http://localhost:5000/debug",
+      "/api/bws/*": {
+        target: "http://localhost:5000",
+        secure: false,
+        changeOrigin: true,
+        logLevel: "debug",
+        pathRewrite: {
+          // Rewrite /api/bws to reflect the api endpoints given in the docker
+          "^/api/bws": "",
+        },
       },
       "/api/*": {
         target: "https://wisdom-demo.uol.de",
