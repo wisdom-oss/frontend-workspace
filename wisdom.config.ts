1e8bded39c9714cc0dd16cdcb053635acab57b89
zoax3155 <stueven.jan@web.de>
2023-09-06 16:09:53 +0200

    corrected typo

--- wisdom.config.ts
+++ wisdom.config.ts
@@ -29,7 +29,7 @@ export const sidebar: SideBarEntries = [
     "Proof of Concept", "rocket", [
       ["Example", "sparkles", "example"],
       ["No Example", null, null],
-      ["Be Water Smart", "sparkles", "be-water-smart"],
+      ["B-Water Smart", "sparkles", "be-water-smart"],
       ["pump-models.label.name", "cube", "pump-models"]
     ]
   ],
