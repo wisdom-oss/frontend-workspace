87cc7a54a7e8a46d8293a6a26a48024cdd2ba72a
zoax3155 <stueven.jan@web.de>
2023-11-06 18:04:48 +0100

    updated navbar
    pls give some feedback

--- wisdom.config.ts
+++ wisdom.config.ts
@@ -8,45 +8,65 @@ import env from "./env";
 export const name = "WISdoM-OSS";
 
 /**
- * This constant is used to inject language content into the translator.
- *
- * The keys to use the injected will use the namespace `sidebar`.
- */
-export const langs = {
-  de_DE: {
-    "water-usage": "Wasserverbrauch"
-  },
-  en_US: {
-    "water-usage": "Water Usage"
-  }
-};
-
-/**
+ * new sidebar with the water cycle headings. 
+ *First iteration, WIP
  * This constant describes how the sidebar should directly look like.
  *
  * For further explanation see {@link SideBarEntries}.
  */
 export const sidebar: SideBarEntries = [
   [
-    "weather-data Category", "sparkles", [
-      ["weather-data Component", "library", "weather-data"]
+    "sidebar.precipitation", "rainy", [
+      ["weather.label.name", "partly-sunny", "weather-data"],
     ]
   ],
   [
-    "Proof of Concept", "rocket", [
-      ["Example", "sparkles", "example"],
-      ["No Example", null, null],
-      ["B-WaterSmart", "sparkles", "be-water-smart"],
-      ["pump-models.label.name", "cube", "pump-models"]
+    "sidebar.groundwater", "earth", [
+      ["Wasserrechte", "file-tray-full", ""]
     ]
   ],
   [
-    "sidebar.water-usage", "bar-chart", [
-      ["water-usage-forecasts.label.name", "calendar", "water-usage-forecasts"]
+    "sidebar.waterworks", "water", [
+      ["pump-models.label.name", "business", "pump-models"],
+      ["water-usage-forecasts.label.name", "bar-chart", "water-usage-forecasts"],
+      ["Predictive Maintenance", "stats-chart", ""]
     ]
-  ]
+  ],
+  [
+    "sidebar.water-usage", "trending-up", [
+      ["B-Water-Smart", "radio", "be-water-smart"]
+    ]
+  ],
+  [
+    "Proof of Concept", "sparkles", [
+      ["example.label.name", "reload-circle", "example"]
+    ]
+  ],
 ];
 
+/**
+ * This constant is used to inject language content into the translator.
+ *
+ * The keys to use the injected will use the namespace `sidebar`.
+ */
+export const langs = {
+  de_DE: {
+    "water-usage": "Wasserverbauch",
+    "precipitation": "Niederschlag",
+    "groundwater": "Grundwasser",
+    "waterworks": "Wasserwerke"
+  },
+  en_US: {
+    "water-usage": "Water Usage",
+    "precipitation": "Precipitation",
+    "groundwater": "Groundwater",
+    "waterworks": "Waterworks"
+  }
+};
+
+/**
+ * constant used to authenticate the credentials provided by the environment variable .env
+ */
 export const authConfig: AuthConfig = {
   authority: env.OIDC_AUTHORITY,
   clientId: env.OIDC_CLIENT_ID
