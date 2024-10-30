94758359cbc890d1fe2bfd4fffee4f39813582ff
Jan <stueven.jan@web.de>
2024-10-30 18:35:33 +0100

    Updated local python path

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,11 @@ module.exports = {
       "/api/takeCsv": {
         target: "http://localhost:8000",
       },
+      // proxy for developing a python api on standard port
+      "/api/localpy/*": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
       "/api/*": {
         target: "https://wisdom-demo.uol.de",
         changeOrigin: true,
